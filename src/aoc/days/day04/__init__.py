a49fec1a5a43035df5a3322143e88d9b353a9841
Jacobo de Vera <devel@jacobodevera.com>
2023-12-06 22:16:51 +0000

    Solve day 4

--- src/aoc/days/day04/__init__.py
+++ src/aoc/days/day04/__init__.py
@@ -1,17 +1,68 @@
 import logging
+import re
+from collections import Counter
+from dataclasses import dataclass
 from pathlib import Path
 
 log = logging.getLogger(__name__)
-_debug = False  ## Whether we are in debug mode
+_debug = False  # Whether we are in debug mode
+
+LINE_PATTERN = re.compile(r"^Card\s+(\d+): (.+) \| (.+)$")
+
+
+def _number_set(numbers: str):
+    return set(int(num) for num in numbers.split())
+
+
+@dataclass
+class Card:
+    id: int
+    numbers: set[int]
+    winning: set[int]
+    matching: set[int]
+    worth: int
+    copies_won: list[int]
+
+    @classmethod
+    def from_line(cls, line: str):
+        m = LINE_PATTERN.match(line)
+        if not m:
+            raise ValueError(f"Invalid line {line}")
+        card_id = int(m.group(1))
+        numbers = _number_set(m.group(2))
+        winning = _number_set(m.group(3))
+        matching = numbers.intersection(winning)
+        worth = 2 ** (len(matching) - 1) if matching else 0
+        copies_won = [card_id + next_card for next_card in range(1, len(matching) + 1)]
+
+        return cls(
+            id=card_id,
+            numbers=numbers,
+            winning=winning,
+            matching=matching,
+            worth=worth,
+            copies_won=copies_won,
+        )
+
+
+def parse_input(input_file: Path):
+    for line in input_file.read_text().splitlines():
+        card = Card.from_line(line)
+        log.debug(f"{card=}")
+        yield card
 
 
 def solve_first(input_file: Path) -> str | None:
-    log.debug(f"{input_file=}")
-    log.error("run_first: Not implemented")
-    return None
+    return str(sum(card.worth for card in parse_input(input_file)))
 
 
 def solve_second(input_file: Path) -> str | None:
-    log.debug(f"{input_file=}")
-    log.error("run_second: Not implemented")
-    return None
+    cards = list(parse_input(input_file))
+    card_counts = Counter()
+    for card in cards:
+        card_counts[card.id] += 1
+        for _ in range(card_counts[card.id]):
+            for copy_id in card.copies_won:
+                card_counts[copy_id] += 1
+
+    return str(sum(card_counts.values()))
--- src/aoc/days/day04/test/test_solve.py
+++ src/aoc/days/day04/test/test_solve.py
@@ -1,36 +1,30 @@
-import pytest
-
 from aoc.days import day04 as today
 
-# TODO: Write the solutions here once you have them verified
-SOLUTIONS: tuple[str | None, ...] = (
-    None,
-    None
-)
+EXAMPLE_INPUT = """
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"""
 
-@pytest.mark.xfail  # TODO: Remove this once implemented
+
 def test_first_example(input_file_factory):
-    input_file = input_file_factory("""
-        TODO: Replace with example for part 1, can be indented 
-        """)
-    assert today.solve_first(input_file) == 'SOLUTION TO PART 1'
+    input_file = input_file_factory(EXAMPLE_INPUT)
+    assert today.solve_first(input_file) == '13'
 
 
-@pytest.mark.xfail  # TODO: Remove this once implemented
 def test_second_example(input_file_factory):
-    input_file = input_file_factory("""
-        TODO: Replace with example for part 2, can be indented 
-        """)
-    assert today.solve_second(input_file) == 'SOLUTION TO PART 2'
+    input_file = input_file_factory(EXAMPLE_INPUT)
+    assert today.solve_second(input_file) == '30'
 
 
-@pytest.mark.xfail  # TODO: Remove this once implemented
-def test_first_solution(day_input):
-    assert SOLUTIONS[0] is not None
-    assert today.solve_first(day_input) == SOLUTIONS[0]
+def test_first_solution(day_input, part1_solution):
+    assert part1_solution is not None
+    assert today.solve_first(day_input) == part1_solution
 
 
-@pytest.mark.xfail  # TODO: Remove this once implemented
-def test_second_solution(day_input):
-    assert SOLUTIONS[1] is not None
-    assert today.solve_second(day_input) == SOLUTIONS[1]
+def test_second_solution(day_input, part2_solution):
+    assert part2_solution is not None
+    assert today.solve_second(day_input) == part2_solution
