01808858a768a53fa01548313945f3e57190d814
Jacobo de Vera <devel@jacobodevera.com>
2023-12-06 21:38:41 +0000

    Move inputs and solution to data directory
    I discovered recently the request by the AoC author to not share inputs
    publicly. In order to support that more easily, I'm separating the
    puzzle input and the solutions for the two parts of each puzzle to their
    own directory (data) so that I can more easily ignore them in git.
    
    The daily tests will now compare results against two fixtures that have
    already read the first line of the part1.solution and part2.solution
    files, respectively.
    
    This has the side effect of making it easier to write solutions in other
    languages and check results outside of the Python tests.

--- src/conftest.py
+++ src/conftest.py
@@ -1,27 +1,58 @@
 from pathlib import Path
 from textwrap import dedent
 import pytest
+
+FILE_NAME_INPUT = 'input'
+FILE_NAME_SOLUTION_PART_1 = 'part1.solution'
+FILE_NAME_SOLUTION_PART_2 = 'part2.solution'
+
+
 @pytest.fixture
 def input_file_factory(tmp_path):
     """
     Creates a temporary input file with the given contents
     """
+
     def mkinput(content):
         assert content[0] == '\n'
         content = dedent(content)[1:]
-        file = tmp_path / 'input'
+        file = tmp_path / FILE_NAME_INPUT
         file.write_text(content)
         return file
+
     yield mkinput
 
+
 @pytest.fixture
-def day_input(request: pytest.FixtureRequest) -> Path:
+def day_data_dir(request: pytest.FixtureRequest) -> Path:
+    return request.path.resolve().parent.parent / 'data'
+
+
+@pytest.fixture
+def day_input(day_data_dir: Path) -> Path:
     """
     Get the file called input that lives in the day module.
     This assumes tests are under a "test" module in the day folder
     """
-    input_file = request.path.resolve().parent.parent / 'input'
+    input_file = day_data_dir / FILE_NAME_INPUT
     assert input_file.exists()
     yield input_file
 
 
+def _get_solution(solution_file: Path):
+    if not solution_file.exists():
+        return None
+    lines = solution_file.read_text().splitlines()
+    if not lines:
+        return None
+    return lines[0]
+
+
+@pytest.fixture
+def part1_solution(day_data_dir: Path) -> str | None:
+    return _get_solution(day_data_dir / FILE_NAME_SOLUTION_PART_1)
+
+
+@pytest.fixture
+def part2_solution(day_data_dir: Path) -> str | None:
+    return _get_solution(day_data_dir / FILE_NAME_SOLUTION_PART_2)
