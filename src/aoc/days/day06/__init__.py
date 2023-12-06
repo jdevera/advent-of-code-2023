5641cd86f63165c72825f4469c7873438ad608d5
Jacobo de Vera <devel@jacobodevera.com>
2023-12-06 22:17:06 +0000

    Solve day 6

--- src/aoc/days/day06/__init__.py
+++ src/aoc/days/day06/__init__.py
@@ -1,17 +1,69 @@
 import logging
+from dataclasses import dataclass
+from enum import Enum, auto
 from pathlib import Path
 
+from cmath import sqrt
+from math import ceil, floor
+
 log = logging.getLogger(__name__)
-_debug = False  ## Whether we are in debug mode
+_debug = False  # Whether we are in debug mode
+
+
+@dataclass
+class Race:
+    time: int
+    distance: int
+
+
+class InputFormat(Enum):
+    MULTI_RACE = auto()
+    SINGLE_RACE = auto()
+
+
+def parse_line(line: str, input_format: InputFormat):
+    label, *values = line.split()
+    if input_format == InputFormat.MULTI_RACE:
+        return [int(val) for val in values]
+    else:
+        return [int("".join(values))]
+
+
+def parse_input(input_file: Path, input_format: InputFormat):
+    time_line, distance_line = input_file.read_text().splitlines()
+    assert time_line.startswith("Time:")
+    assert distance_line.startswith("Distance:")
+    times = parse_line(time_line, input_format)
+    distances = parse_line(distance_line, input_format)
+
+    assert len(times) == len(distances)
+    return [Race(time, distance) for time, distance in zip(times, distances)]
+
+
+def solve_for_x(a, b, c):
+    return [(-b + factor * sqrt(b ** 2 - 4 * a * c)) / -2 for factor in [1, -1]]
+
+
+def ways_to_win_power(input_file: Path, input_format: InputFormat) -> str | None:
+    races = parse_input(input_file, input_format)
+    log.debug(f"{races=}")
+    total = 1
+    for race in races:
+        log.debug("Checking race: %s", race)
+        edges = solve_for_x(a=-1, b=race.time, c=-race.distance)
+
+        log.debug("edges %s", edges)
+        start_winning = floor(edges[0].real) + 1
+        start_losing = ceil(edges[1].real)
+
+        total *= (start_losing - start_winning)
+
+    return str(total)
 
 
 def solve_first(input_file: Path) -> str | None:
-    log.debug(f"{input_file=}")
-    log.error("run_first: Not implemented")
-    return None
+    return ways_to_win_power(input_file, InputFormat.MULTI_RACE)
 
 
 def solve_second(input_file: Path) -> str | None:
-    log.debug(f"{input_file=}")
-    log.error("run_second: Not implemented")
-    return None
+    return ways_to_win_power(input_file, InputFormat.SINGLE_RACE)
--- src/aoc/days/day06/test/test_solve.py
+++ src/aoc/days/day06/test/test_solve.py
@@ -1,36 +1,26 @@
-import pytest
-
 from aoc.days import day06 as today
 
-# TODO: Write the solutions here once you have them verified
-SOLUTIONS: tuple[str | None, ...] = (
-    None,
-    None
-)
+EXAMPLE_INPUT = """
+    Time:      7  15   30
+    Distance:  9  40  200
+    """
 
-@pytest.mark.xfail  # TODO: Remove this once implemented
+
 def test_first_example(input_file_factory):
-    input_file = input_file_factory("""
-        TODO: Replace with example for part 1, can be indented 
-        """)
-    assert today.solve_first(input_file) == 'SOLUTION TO PART 1'
+    input_file = input_file_factory(EXAMPLE_INPUT)
+    assert today.solve_first(input_file) == '288'
 
 
-@pytest.mark.xfail  # TODO: Remove this once implemented
 def test_second_example(input_file_factory):
-    input_file = input_file_factory("""
-        TODO: Replace with example for part 2, can be indented 
-        """)
-    assert today.solve_second(input_file) == 'SOLUTION TO PART 2'
+    input_file = input_file_factory(EXAMPLE_INPUT)
+    assert today.solve_second(input_file) == '71503'
 
 
-@pytest.mark.xfail  # TODO: Remove this once implemented
-def test_first_solution(day_input):
-    assert SOLUTIONS[0] is not None
-    assert today.solve_first(day_input) == SOLUTIONS[0]
+def test_first_solution(day_input, part1_solution):
+    assert today.solve_first(day_input) == part1_solution
+    assert part1_solution is not None
 
 
-@pytest.mark.xfail  # TODO: Remove this once implemented
-def test_second_solution(day_input):
-    assert SOLUTIONS[1] is not None
-    assert today.solve_second(day_input) == SOLUTIONS[1]
+def test_second_solution(day_input, part2_solution):
+    assert today.solve_second(day_input) == part2_solution
+    assert part2_solution is not None
